07f10ca4e15f6e78878bf1113c55c1795e24e292
DAVIDleo97 <vasquezleo1997@gmail.com>
2023-05-29 02:26:03 -0500

    primer_commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <ImageBackground source={require('./assets/fondo1.png' )} resizeMode='cover' style={styles.image}>
+        <ImageBackground style={styles.img}
+        source={require('./assets/fondo2.png' )}>
+          <Image style={styles.imgUser} source={require('./assets/usuario.png' )}/>
+          <Text style={styles.tetx}>Open up App.js to start!</Text>
+        </ImageBackground>
+        
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +22,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8080ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  tetx: {
+    justifyContent: 'center'
+  },
+  img:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 350,
+    height: 500,
+  },
+  imgUser:{
+    top: -250,
+    width: 100,
+    height: 100,
+  }
+
+  
 });
